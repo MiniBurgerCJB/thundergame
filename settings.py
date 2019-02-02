aac963962bf347a874712944a48f5e0373ea257e
MiniBurgerCJB <877080200@qq.com>
2019-02-02 19:58:07 +0800

    first commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """  储存游戏的所有设置的类"""
+
+    def __init__(self):
+        """ 初始化游戏设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 600
+        # 设置背景色
+        self.bg_color = (230, 230, 230)
