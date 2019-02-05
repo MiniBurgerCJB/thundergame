5b27f81548fda1d6e5030a47e275e47bb3123e9d
MiniBurgerCJB <877080200@qq.com>
2019-02-05 15:29:25 +0800

    add 一行外球人

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,6 @@ import game_functions as gf  # 给导入的模块game_functions 指定了别名g
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 from pygame.sprite import Group
 
 
@@ -19,15 +18,17 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
-    # 创建一个外球人
-    alien = Alien(ai_settings, screen)
+    # 创建一个外球人的编组
+    aliens = Group()
+    # 创建外球人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -45,7 +46,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     # 更新屏幕上的图像，并切换到新屏幕
     # 每次循环都重新绘制屏幕
     screen.fill(ai_settings.bg_color)
@@ -53,7 +54,7 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
@@ -67,3 +68,21 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+
+def create_fleet(ai_settings, screen, aliens):
+    """创建外球人群"""
+    # 创建一个外球人，并计算一行能容纳多少外球人
+    # 外球人间距外外球人宽度
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    # 创建第一行外球人
+    for alien_number in range(number_aliens_x):
+        # 创建一个外球人并将其加入当前行
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
