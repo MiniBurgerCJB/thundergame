e6d19bf0196e49a77c53b293b94bd225c82c897a
MiniBurgerCJB <877080200@qq.com>
2019-02-06 18:30:16 +0800

    add playay_button

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import game_functions as gf  # 给导入的模块game_functions 指定了别名g
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from pygame.sprite import Group
 
@@ -15,6 +16,8 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
     # 创建一艘飞船
@@ -28,14 +31,14 @@ def run_game():
 
     # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-            
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
